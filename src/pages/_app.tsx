3951d8a1be5f4403d8738e4357dbee144bd64a64
peter <petersantoso94@gmail.com>
2020-09-07 21:16:12 +0800

    next-urql integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,105 +1,17 @@
-import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
-import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
-import {
-  cacheExchange,
-  Cache,
-  QueryInput,
-  query,
-} from "@urql/exchange-graphcache";
+import { ColorModeProvider, CSSReset, ThemeProvider } from "@chakra-ui/core";
 import theme from "../theme";
-import { graphqlUrl } from "../Constants";
-import {
-  MeDocument,
-  LoginMutation,
-  MeQuery,
-  RegisterMutation,
-  LogoutMutation,
-} from "../generated/graphql";
-
-function UpdateQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
-
-const client = createClient({
-  url: graphqlUrl,
-  fetchOptions: {
-    credentials: "include",
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          login: (_result, args, cache, info) => {
-            UpdateQuery<LoginMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.login.errors) {
-                  return query;
-                } else {
-                  return {
-                    me: result.login.user,
-                  };
-                }
-              }
-            );
-          },
-          register: (_result, args, cache, info) => {
-            UpdateQuery<RegisterMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                if (result.register.errors) {
-                  return query;
-                } else {
-                  return {
-                    me: result.register.user,
-                  };
-                }
-              }
-            );
-          },
-          logout: (_result, args, cache, info) => {
-            UpdateQuery<LogoutMutation, MeQuery>(
-              cache,
-              { query: MeDocument },
-              _result,
-              (result, query) => {
-                console.log(result);
-                if (result.logout) {
-                  return { me: null };
-                } else {
-                  return query;
-                }
-              }
-            );
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-});
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/utils";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider value={client}>
-      <ThemeProvider theme={theme}>
-        <ColorModeProvider>
-          <CSSReset />
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <ColorModeProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ThemeProvider>
   );
 }
 
-export default MyApp;
+export default withUrqlClient(createUrqlClient)(MyApp);
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,20 @@
-import { CustomError } from "../generated/graphql";
+import {
+  CustomError,
+  LoginMutation,
+  MeQuery,
+  MeDocument,
+  RegisterMutation,
+  LogoutMutation,
+} from "../generated/graphql";
+import { SSRExchange } from "next-urql";
+import { graphqlUrl } from "../Constants";
+import { dedupExchange, fetchExchange } from "urql";
+import {
+  QueryInput,
+  Cache,
+  query,
+  cacheExchange,
+} from "@urql/exchange-graphcache";
 
 export const toErrorMap = (errors: CustomError[]): Record<string, string> => {
   const errorMap: Record<string, string> = {};
@@ -8,3 +24,77 @@ export const toErrorMap = (errors: CustomError[]): Record<string, string> => {
 
   return errorMap;
 };
+
+function UpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
+
+export const createUrqlClient = (ssrExchange: SSRExchange) => ({
+  url: graphqlUrl,
+  fetchOptions: {
+    credentials: "include",
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            UpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            UpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+          logout: (_result, args, cache, info) => {
+            UpdateQuery<LogoutMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                console.log(result);
+                if (result.logout) {
+                  return { me: null };
+                } else {
+                  return query;
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    ssrExchange,
+    fetchExchange,
+  ],
+});
