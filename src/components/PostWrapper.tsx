8911b3dfb39b0100ac247c99d840a8fef5aa93e3
peter <petersantoso94@gmail.com>
2020-10-11 02:12:39 +0800

    UPVOTE

--- src/components/PostWrapper.tsx
+++ src/components/PostWrapper.tsx
@@ -1,13 +1,19 @@
-import React from "react";
 import {
-  SimpleGrid,
-  Box,
   Accordion,
-  AccordionItem,
   AccordionHeader,
   AccordionIcon,
+  AccordionItem,
   AccordionPanel,
+  Box,
+  Button,
+  Flex,
+  Grid,
+  SimpleGrid,
+  Text,
 } from "@chakra-ui/core";
+import moment from "moment";
+import React from "react";
+import { MdArrowDownward, MdArrowUpward } from "react-icons/md";
 import { Post } from "../generated/graphql";
 
 type Prop = {
@@ -22,15 +28,46 @@ export const PostWrapper = (prop: Prop) => {
     <Accordion allowToggle>
       <SimpleGrid columns={column} spacing={spacing}>
         {posts.map((x, idx) => (
-          <AccordionItem key={x.title + idx + x.createdAt}>
-            <AccordionHeader>
-              <Box flex="1" textAlign="left">
-                {x.title}
-              </Box>
-              <AccordionIcon />
-            </AccordionHeader>
-            <AccordionPanel pb={4}>{x.content}</AccordionPanel>
-          </AccordionItem>
+          <Flex w="100%">
+            <Box w="5%">
+              <Button
+                leftIcon={MdArrowUpward}
+                variantColor="red"
+                variant="outline"
+                pr={-2}
+                onClick={() => {}}
+              >
+                {}
+              </Button>
+              <Text textAlign="center">
+                <strong>{x.point}</strong>
+              </Text>
+              <Button
+                leftIcon={MdArrowDownward}
+                variantColor="red"
+                variant="outline"
+                pr={-2}
+                onClick={() => {}}
+              >
+                {}
+              </Button>
+            </Box>
+            <Box w="100%">
+              <AccordionItem key={x.title + idx + x.createdAt}>
+                <AccordionHeader>
+                  <Box flex="1" textAlign="left">
+                    <Text color="black.200" isTruncated as="b">
+                      {x.title}
+                    </Text>
+                    {` - Posted by ${x.user.username} 
+                ${moment(new Date(parseFloat(x.createdAt))).fromNow()}`}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionHeader>
+                <AccordionPanel pb={4}>{x.content}</AccordionPanel>
+              </AccordionItem>
+            </Box>
+          </Flex>
         ))}
       </SimpleGrid>
     </Accordion>
